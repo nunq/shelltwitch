6e7b3d9e3804bf1d44c2804f9cceef47da74b72d
hyphenc <46054695+hyphenc@users.noreply.github.com>
2020-06-09 23:53:45 +0200

    minor fixes. ready to PR & rebase

--- shelltwitch.sh
+++ shelltwitch.sh
@@ -5,7 +5,7 @@ set -e #exit on error
 CLIENTID="" #YOUR_CLIENTID
 CLIENTSECRET="" #YOUR_CLIENTSECRET
 USER="" #YOUR_USERNAME
-ENABLEDELAY="" #add 1s delay if online streamers >5 ? 1=yes, 0=no
+ENABLEDELAY="1" #add 1s delay if online streamers >5 ? 1=yes, 0=no
 CACHEDIR="$HOME/.cache/shelltwitch"
 
 main() {
@@ -17,7 +17,7 @@ main() {
 update() {
     mapfile -t streamers < "$CACHEDIR"/streamers
     for streamer in "${streamers[@]}"; do
-        jsonData=$(curl -s -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/streams?user_login=$streamer")
+        jsonData=$(curl -s -H "Client-ID: $CLIENTID" -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/streams?user_login=$streamer")
         #if the twitch api says the streamer is live, add them to oStreamers[]
         if [ "$(echo "$jsonData" | grep -Po '"type":.*?[^\\]",')" == '"type":"live",' ]; then
             oStreamers+=("$streamer")
@@ -28,15 +28,15 @@ update() {
 getMetadata() {
     #get some metadata like the stream title or what game is being played
     if [ "$ENABLEDELAY" == "1" ] && [ "${#oStreamers[@]}" -gt "5" ]; then sleep 1; fi
-    gameid=$(curl -s -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/streams?user_login=$1" | grep -Po '"game_id":.*?[^\\]",' | sed 's/^"game_id":"//i;s/",$//i')
+    gameid=$(curl -s -H "Client-ID: $CLIENTID" -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/streams?user_login=$1" | grep -Po '"game_id":.*?[^\\]",' | sed 's/^"game_id":"//i;s/",$//i')
     if ! [ $(grep -o "$gameid" "$CACHEDIR"/gameids ) ]; then
-        game=$(printf "%b" "$(curl -s -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/games?id=$gameid" | grep -Po '"name":.*?[^\\]",' | sed 's/^"name":"//i;s/",$//i')")
+        game=$(printf "%b" "$(curl -s -H "Client-ID: $CLIENTID" -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/games?id=$gameid" | grep -Po '"name":.*?[^\\]",' | sed 's/^"name":"//i;s/",$//i')")
         if [ -n "$game" ]; then echo "$gameid: \"$game\"" >> "$CACHEDIR"/gameids; fi
     else
         game=$(grep -oP "(?<=$gameid: \").*?(?=\"$)" "$CACHEDIR"/gameids)
     fi
     if [ -z "$game" ]; then title="couldn't get game (rate limiting)"; fi
-    title=$(printf "%b" "$(curl -s -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/streams?user_login=$1" | grep -Po '"title":.*?[^\\]",' | sed 's/^"title":"//i;s/",$//i')")
+    title=$(printf "%b" "$(curl -s -H "Client-ID: $CLIENTID" -H "Authorization: Bearer $OAUTHTOKEN" "https://api.twitch.tv/helix/streams?user_login=$1" | grep -Po '"title":.*?[^\\]",' | sed 's/^"title":"//i;s/",$//i')")
     if [ -z "$title" ]; then title="couldn't get stream title (rate limiting)"; fi
 }
 
@@ -45,8 +45,8 @@ updateCachedStreamers() {
     echo -n "" > "$CACHEDIR"/gameids
     #clear cached streamers
     echo -n "" > "$CACHEDIR"/streamers
-    USERID=$(curl -s -H "Authorization: Bearer $OAUTHTOKEN" https://api.twitch.tv/helix/users?login="$USER"| grep -Po '"id":.*?[^\\]",' | sed 's/^"id":"//i;s/",$//i')
-    mapfile -t streamers <<< $(curl -s -H "Authorization: Bearer $OAUTHTOKEN" https://api.twitch.tv/helix/users/follows?from_id="$USERID" | grep -Po '"to_name":.*?[^\\]",' | sed 's/^"to_name":"//;s/",$//i')
+    USERID=$(curl -s -H "Client-ID: $CLIENTID" -H "Authorization: Bearer $OAUTHTOKEN" https://api.twitch.tv/helix/users?login="$USER"| grep -Po '"id":.*?[^\\]",' | sed 's/^"id":"//i;s/",$//i')
+    mapfile -t streamers <<< $(curl -s -H "Client-ID: $CLIENTID" -H "Authorization: Bearer $OAUTHTOKEN" https://api.twitch.tv/helix/users/follows?from_id="$USERID" | grep -Po '"to_name":.*?[^\\]",' | sed 's/^"to_name":"//;s/",$//i')
     #cache followed streamers
     for streamer in "${streamers[@]}"; do
         echo "$streamer" >> "$CACHEDIR"/streamers
@@ -97,7 +97,7 @@ prepNotify() {
 
 # acquire oauth token
 getoauthtoken() {
-  curl -s -X POST "https://id.twitch.tv/oauth2/token?client_id=$CLIENTID&client_secret=$CLIENTSECRET&grant_type=client_credentials" | grep -oP '(?<="access_token":").*?(?=",")' > "$CACHEDIR"/oauth-token
+  curl -s -X POST "https://id.twitch.tv/oauth2/token?client_id=$CLIENTID&client_secret=$CLIENTSECRET&grant_type=client_credentials" | grep -oP '(?<="access_token":").*?(?=",")' > "$CACHEDIR"/token
   [[ -s "$CACHEDIR"/token ]] && echo "saved oauth token to "$CACHEDIR"/token"
 }
 
@@ -135,23 +135,24 @@ if [ ! -d "$CACHEDIR" ]; then mkdir -p "$CACHEDIR"; fi
 if [ ! -f "$CACHEDIR"/live ]; then touch "$CACHEDIR"/live; fi
 if [ ! -f "$CACHEDIR"/streamers ]; then touch "$CACHEDIR"/streamers; fi
 OAUTHTOKEN="$(head -n 1 "$CACHEDIR"/token)" || OAUTHTOKEN=""
-[[ -z "$OAUTHTOKEN" ]] && echo -e "error: no oauth token found\nplease run this script with 'oauth' to get a valid oauth token" && exit 1 
 
 case $1 in
-    cron)
-        #environment vars for notify-send
-        export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
-        export DISPLAY=":0"
-        prepNotify
-        shouldNotify ;;
-    upcache)
-        updateCachedStreamers ;;
-    oauth)
-        getoauthtoken ;;
-    checktoken)
-        checktoken ;;
-    h|help|--help|-help)
-        printhelp ;;
-    *)
-        main ;;
+	cron)
+		[[ -z "$OAUTHTOKEN" ]] && echo -e "error: no oauth token found\nplease run this script with 'oauth' to get a valid oauth token" && exit 1
+		#environment vars for notify-send
+		export DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/1000/bus"
+		export DISPLAY=":0"
+		prepNotify
+		shouldNotify ;;
+	upcache)
+		[[ -z "$OAUTHTOKEN" ]] && echo -e "error: no oauth token found\nplease run this script with 'oauth' to get a valid oauth token" && exit 1
+		updateCachedStreamers ;;
+	oauth)
+		getoauthtoken ;;
+	checktoken)
+		checktoken ;;
+	h|help|--help|-help)
+		printhelp ;;
+	*)
+		main ;;
 esac
